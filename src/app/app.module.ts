8603a16519e3c2d3552e590435c4048a69803573
Jon Provan <jprovan@skillstorm.com>
2022-12-22 09:23:18 -0500

    Jon's First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DadJokesComponent } from './dad-jokes/dad-jokes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DadJokesComponent
   ],
   imports: [
     BrowserModule
